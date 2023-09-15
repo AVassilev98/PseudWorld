f8e016d9a54b6783ca0d60cba19b7ece7cbefda8
Anton Vassilev <avvassil@uwaterloo.ca>
2023-09-15 01:09:22 -0500

    Add login functionality

--- src/LoginPage/LoginPage.tsx
+++ src/LoginPage/LoginPage.tsx
@@ -1,48 +1,192 @@
 import React from "react";
 import "./LoginPage.scss"
+import { Auth } from 'aws-amplify';
+import { Navigate } from "react-router-dom";
+
+const logo = String.raw
+    `••▀▀▀▀••▪                                                                              
+▪•▀•••••▀▀▀•••                                                                         
+.▀•..•▄▄▄▄▄▀▀▀▀▀••                                                                     
+ ▪▀•▪▀▄••▪▪••▀▀▀▄▀▀▀•▪                                                                 
+  ▪▀••▄•.▪••▪▪••••▀▀▄▄▀▀•                                                              
+   .••▀▄•▪•▪•••••••••▀▀▀▄▀▀•                                                           
+     •▀•▄▀•••••▄▄▄▄▀▀▀▀••▀▀▄▄▀•    .••••••••••▪.                                       
+      .•▀▀▀••••▄▀•••••▀▀▀▀▀•▀▀▄▄•••••••••••▀▀▀▀▀▄▀▀•▪                                  
+        •▀▀▀▀▀•▀▄▀•••..▪▪•▀▀▀••••▪.▪••▪.•••••▀▀•▀▀▀▄▄▀▀•                               
+          •▀▀▀▀••▄••▪     .▪•••..▪•. .••..•••••▀▀▀▀▀▀▀▄▄▄▀•                            
+            •▀▀▀••▄▀••....▪▪▪▪•••..▪▪▪..••▪▪••••••▀▀▀▀▀▀▄▄▄▀▪                          
+             .•▀▀▀•▀▄▀•••▪...▪••••▪▪..▪▪..•••▪••••••▀▀▀▀▀▄▄▄▄▀.                        
+               .▀▀▄▀•▀▀▀▀▀▪....▪▪••••....▪▪▪•••••••••▀▀▀▀▀▀▄▄▄▄▪                       
+                 .▀▀▄▀▀▀▀▀▀•.......••••.....▪•••••••▀▀▀▀▀▀▀▀▄▄▄▄•                      
+                   .▀•••▀▀▄▀▀•▪..▪▪..•••▪..▪••••••••••▀▀▀▀▀▀▀▄▄▄█▪                     
+                    ▪ ••••▀▀▄▄▀•▪...▪▪.▪•••▪▪•••••••••▀▀▀▀▀▀▀▀▀▄█▄                     
+                    •.  •▀•••▀▄▄▀•▪. .▪▪..•▀••••••••▀▀••▀▀▀▀▀▀▄▄▄█•                    
+                    • . . •▀▀••▀▄▄▀•▪▪  ▪••••▀▀•••••••▀▀▀▀▀▀▀▀▀▄▄█▄                    
+                    •.  ..  ••▀▀▀▀▄▄▄▀••▪••••••▀▀▀▀•▀▀▀▀▀▀▀▀▀▀▀▄▄██                    
+                    ••..  .   .•▀▀▀▀▄▄▄▀••••••••••▀▀▀▀▀▀▀▀▀▀▀▀▄▄▄██                    
+                     ▀•... .     •▀▀▀▀▀▄▄▄▀•••••••••▀▀▀▀▀▀▀▀▀▄▄▄▄█▄                    
+                     .▀•▪.▪...     ▪•▀▀▀▀▄▄▄▀▀•••••••▀▀▀▀▀▀▀▄▄▄▄██•                    
+                      ▪▀▀•▪▪▪..•▪   ..•▀▀▀▀▀▄▄▄▀▀•••••▀▀▀▀▀▀▄▄▄▄█▄                     
+                       .▀▄•••••▪...   .▪••▀▀▀▀▀▄█▄▀▀▀▀▀▀▀▀▄▀▄▄▄█▄.   ••▀▪              
+                         •▄▄▀•••••▪•••▪.▪•▪•▀▀▀▀▀▄▄█▄▄▄▄▀▀▄▄▄██▄▪.•▀▀▀▄▄▄▀▪            
+                          .•▄▄▄▀••▀•••••••••••▀▀▀▀▀▀▄▄▄▄▄▄▄▄██▀  .▀▄▄▀▀▀▄▄▄▀.          
+                             •▄▄▄▄▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▄▄▄██▄▪     ▀▄▄▄▀▀▄▄▄▄•.        
+                               .▀▀▄▄▄▄▄▀▀▀▀▀▀▀▀▄▄▄▄▄▄▄▄▄▄▀▀▄▄▄▄▀▀▀▀▀▄▄▄▄▀▀▀▄▄▄▄•       
+                                  .•▀▀▄▄▄█▄▄▄▄█████▄▄▄▀▀▄▄▄▄▀▀▀▄▄▄▄▄▄▄▄▀▀▀▀▀▀▄▄▄▀▪     
+                                         ▪•••••••▪.      •▀▄▄▄▄▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▄▄▄▄▀    
+                                                            ▪▀▄▄▄▄▄▄▀▀▀▀▀▀▀▀▀▀▀▄▄▄▄▄•  
+                                                               .•▀▄▄▄▄▄▄▀▀▀▀▀▀▀▀▄▀▄▄▄• 
+                                                                   •▀▀▄▄▄▄▄▄▄▄▄▄█▀▀▄▄▄•
+                                                                       •▀▀▄▄▄▄▄▄▄▄▄▄▄▄▄
+                                                                           ▪•▀▀▄▄▄▄▄▄▄▄
+                                                                                .••••• `;
 
 type LoginPageState = {
     signingUp: boolean
+    username: string
+    password: string
+    secretKey: string
+    errorMessage: string
+    signedIn: boolean
 }
 
-class LoginPage extends React.Component<{}, LoginPageState> {
-    constructor(props: {}) {
+type LoginPageProps = {
+    onUserSigninSuccess: Function,
+}
+
+type SignUpParameters = {
+    username: string;
+    password: string;
+    secretKey: string;
+};
+
+export async function SendSignupRequest({ username, password, secretKey }: SignUpParameters) {
+    try {
+        const { user } = await Auth.signUp({
+            username: username,
+            password: password,
+            attributes: {
+                "custom:secretKey": secretKey,
+            },
+            autoSignIn: {
+                enabled: true,
+            },
+        });
+        return Promise.resolve(user);
+
+    } catch (error) {
+        return Promise.reject(error);
+    }
+}
+
+type SignInParameters = {
+    username: string;
+    password: string;
+};
+
+export async function SendSigninRequest({ username, password }: SignInParameters) {
+    try {
+        const { user } = await Auth.signIn({
+            username: username,
+            password: password,
+        });
+        return Promise.resolve(user);
+    }
+    catch (error) {
+        return Promise.reject(error);
+    }
+}
+
+class LoginPage extends React.Component<LoginPageProps, LoginPageState> {
+    constructor(props: LoginPageProps) {
         super(props);
         this.state = {
             signingUp: false,
+            username: "",
+            password: "",
+            secretKey: "",
+            errorMessage: "",
+            signedIn: false,
         };
-        this.signup = this.signup.bind(this);
+        this.clickSigninButton = this.clickSigninButton.bind(this);
+        this.clickSignupButton = this.clickSignupButton.bind(this);
     }
 
-    signup() {
-        this.setState({ signingUp: true })
+    clickSignupButton() {
+        if (!this.state.signingUp) {
+            this.setState({ signingUp: true })
+            return;
+        }
+
+        SendSignupRequest({ username: this.state.username, password: this.state.password, secretKey: this.state.secretKey })
+            .then(
+                () => {
+                    this.props.onUserSigninSuccess(true);
+                    this.setState({ signedIn: true })
+                },
+                (err) => {
+                    this.setState({ errorMessage: err.message });
+                }
+            );
+    }
+
+    clickSigninButton() {
+        SendSigninRequest({ username: this.state.username, password: this.state.password })
+            .then(
+                () => {
+                    this.props.onUserSigninSuccess(true);
+                    this.setState({ signedIn: true })
+                },
+                (err) => {
+                    this.setState({ errorMessage: err.message });
+                }
+            );
     }
 
     render() {
         const isSigningUp = this.state.signingUp;
         let loginButton;
         let signupTextArea;
+        let errorMessageBlock;
 
         if (isSigningUp) {
-            signupTextArea = <textarea className="loginText" placeholder="Signup Keyword"></textarea>;
+            signupTextArea = <textarea className="loginText" placeholder="Signup Keyword"
+                onChange={(e) => { this.setState({ secretKey: e.target.value }) }}>
+            </textarea>;
         }
         else {
-            loginButton = <a className="loginButton  clickable"> [ Login ] </a>;
+            loginButton = <a className="loginButton  clickable" onClick={this.clickSigninButton}> [ Login ] </a>;
+        }
+
+        if (this.state.errorMessage != "") {
+            errorMessageBlock = <a className="loginMessageText">{this.state.errorMessage}</a>
         }
 
         return (
-            <div className="loginContainerOuter">
-                <div className="loginContainerInner">
-                    <div>
-                        <textarea className="loginText" placeholder="Username"></textarea>
-                    </div>
-                    <div>
-                        <textarea className="loginText" placeholder="Password"></textarea>
-                    </div>
-                    {signupTextArea}
-                    <div className="loginButtonContainer">
-                        {loginButton}
-                        <a className="loginButton clickable" onClick={this.signup}> [ Signup ]</a>
+            <div style={{ width: "100vw", height: "100vh", display: "flex" }}>
+                <div className="outerContainer">
+                    <pre className="logo">{logo}</pre>
+                    <div className="loginContainerOuter">
+                        <div className="loginContainerInner">
+                            <a className="loginMessageText"> Welcome To Club Pseud World</a>
+                            <div>
+                                <textarea className="loginText" placeholder="Username"
+                                    onChange={(e) => { this.setState({ username: e.target.value }) }}>
+                                </textarea>
+                            </div>
+                            <div>
+                                <textarea className="loginText" placeholder="Password"
+                                    onChange={(e) => { this.setState({ password: e.target.value }) }}>
+                                </textarea>
+                            </div>
+                            {signupTextArea}
+                            <div className="loginButtonContainer">
+                                {loginButton}
+                                <a className="loginButton clickable" onClick={this.clickSignupButton}> [ Signup ]</a>
+                            </div>
+                            {errorMessageBlock}
+                        </div>
+                        {this.state.signedIn && <Navigate to={'/'}></Navigate>}
                     </div>
                 </div>
             </div>
