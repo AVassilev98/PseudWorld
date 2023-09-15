e0e5a95447b8d8c5d74b6a2377c37ebc2d8a1903
Anton Vassilev <avvassil@uwaterloo.ca>
2023-09-15 01:33:55 -0500

    Fix lambda function

--- amplify/backend/function/pseudWorldAuthPreSignup/src/index.js
+++ amplify/backend/function/pseudWorldAuthPreSignup/src/index.js
@@ -1,33 +1,23 @@
-/**
- * @fileoverview
- *
- * This CloudFormation Trigger creates a handler which awaits the other handlers
- * specified in the `MODULES` env var, located at `./${MODULE}`.
- */
+exports.handler = (event, context, callback) => {
+  
+  const secretKey = "DepLoveParadise";
+  
+  if (!event.request.userAttributes.hasOwnProperty("custom:secretKey") ||
+      event.request.userAttributes["custom:secretKey"] !== secretKey)
+  {
+    var error = new Error("Incorrect secret key");
+    callback(error, event);
+  }
+  
+  event.response.autoConfirmUser = true;
+  
+  if (event.request.userAttributes.hasOwnProperty("email")) {
+    event.response.autoVerifyEmail = true;
+  }
+  
+  if (event.request.userAttributes.hasOwnProperty("phone_number")) {
+    event.response.autoVerifyPhone = true;
+  }
 
-/**
- * The names of modules to load are stored as a comma-delimited string in the
- * `MODULES` env var.
- */
-const moduleNames = process.env.MODULES.split(',');
-/**
- * The array of imported modules.
- */
-const modules = moduleNames.map((name) => require(`./${name}`));
-
-/**
- * This async handler iterates over the given modules and awaits them.
- *
- * @see https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html#nodejs-handler-async
- * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
- *
- */
-exports.handler = async (event, context) => {
-  /**
-   * Instead of naively iterating over all handlers, run them concurrently with
-   * `await Promise.all(...)`. This would otherwise just be determined by the
-   * order of names in the `MODULES` var.
-   */
-  await Promise.all(modules.map((module) => module.handler(event, context)));
-  return event;
+  callback(null, event);
 };
